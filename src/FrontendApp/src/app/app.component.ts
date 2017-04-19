e6938e45216e2dd9c3fa4096d6e4fe8aa25b49db
Baroo <baroo.phone@live.com>
2017-04-19 19:51:03 +0200

    Changed default locales for Angular2 application to pl-PL

--- src/FrontendApp/src/app/app.component.ts
+++ src/FrontendApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
-﻿import { Component } from '@angular/core';
-
+﻿import { Component, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: LOCALE_ID, useValue: 'pl-PL' }]
 })
+
 export class AppComponent {
 
 }
